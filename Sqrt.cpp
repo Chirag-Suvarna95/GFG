fa237339213a3d364ce1868f86a367da9b786e27
chirag <chiragsuvarna95@gmail.com>
2024-07-16 22:48:07 +0530

    1st commit

---
+++ Sqrt.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        // Your code goes here   
+        
+     for(int i=1;i<=x;i++)
+     {
+         if(i*i==x)return i;
+         else if(i*i>x) return i-1;
+     }
+      return x;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		Solution obj;
+		cout << obj.floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+
+// } Driver Code Ends
