096a7b545ce41beaaa2675dc7c6d6190fcf58730
chirag <chiragsuvarna95@gmail.com>
2024-07-23 20:56:21 +0530

    Given two binary trees, the task is to find if both of them are identical or not

---
+++ IdenticalTrees.cpp
@@ -0,0 +1,34 @@
+/* A binary tree node
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+   
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+        if( (!r1 && !r2) || 
+            (r1 && r2 && (r1->data == r2->data) && 
+            isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right)))
+          return true;
+          else return false;
+          
+          
+    }
+};
