76f9feb53dcc7f32d7d00964e4d6f1e6c630fa54
chirag <chiragsuvarna95@gmail.com>
2024-07-29 21:45:31 +0530

    Anagram

---
+++ Anagram.cpp
@@ -0,0 +1,17 @@
+
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b){
+        
+        // Your code here
+       if(a.length() != b.length())
+       return false;
+       sort(a.begin(),a.end());
+       sort(b.begin(),b.end());
+       return a==b;
+        
+    }
+
+};
