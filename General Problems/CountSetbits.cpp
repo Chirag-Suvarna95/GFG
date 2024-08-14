6f894f308c56d08ea5ca9a71cb4b7b39b9f8ae10
chirag <chiragsuvarna95@gmail.com>
2024-08-14 22:20:47 +0530

    2nd Solution for->Number of 1 Bits

--- General Problems/CountSetbits.cpp
+++ General Problems/CountSetbits.cpp
@@ -14,3 +14,27 @@ class Solution {
         return setbits;
     }
 };
+
+/*Brian made a wonderful observation that when we subtract a number by 1 
+then all the bits after and at the rightmost set bit gets flipped.
+ */
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int setbits=0;
+      while(N>0)
+       {
+          N=N&(N-1);  /*this will unset the rightmost set bit
+          and assign the new number to N.*/
+          /*N = 12. In binary 1100.
+
+          N = 1100
+          N - 1 = 1011  ->Unsets the rightmost setbits
+          Bitwise AND: 1100 & 1011 = 1000
+*/
+          setbits++;  
+       }
+        return setbits;
+    }
+};
