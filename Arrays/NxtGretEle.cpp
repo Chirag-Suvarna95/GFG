daab6017a2a5b4720819ed01e3111c75fbaffb2e
chirag <chiragsuvarna95@gmail.com>
2024-08-27 22:27:58 +0530

    Given an array arr[ ] of n elements, the task is to find the next greater element for each element of the array in order of their appearance in the array-> Brute Froce

---
+++ Arrays/NxtGretEle.cpp
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+/*Brute Force-> Failed due to long Time complexity*/
+
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        int i=0;
+        int length=arr.size();
+        while(i!=n)
+        { 
+            int j=i+1;
+            while(arr[i]>arr[j] && j!=n)
+              { 
+                  if(j==n-1)
+                  {
+                      arr[i]=-1;
+                      goto here;
+                  }
+                  j++;
+              }
+            arr[i]=arr[j];
+            here:
+            i++;
+            
+        }
+        arr[i-1]=-1;
+        return arr;
+    }
+};
