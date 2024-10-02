f4fb95276f18b9a4a21cb1ab1dc08966b3cc617c
Chirag <chiragsuvarna95@gmail.com>
2024-10-02 12:37:58 +0530

    Second Largest Optimal Solution

--- Arrays/2ndLargest.cpp
+++ Arrays/2ndLargest.cpp
@@ -43,3 +43,29 @@ class Solution {
        return SLarge;
     }
 };
+
+//--------------------------Optimal Solution------------------------------
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int SLarge=-1;
+        int Large=arr[0];
+       for(int i=0;i<arr.size();i++)
+       {
+           if(arr[i]>Large)
+           {
+               SLarge=Large;
+               Large=arr[i];
+               
+           }
+           else if(arr[i]>SLarge && arr[i]!=Large){
+               SLarge=arr[i];
+           }
+       }
+      
+       return SLarge;
+    }
+};
