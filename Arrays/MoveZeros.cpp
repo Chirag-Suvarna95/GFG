a2a1727b236e92132a26090725d30270180c0c50
Chirag <chiragsuvarna95@gmail.com>
2024-10-04 22:24:15 +0530

    Introduction to Doubly Linked List

---
+++ Arrays/MoveZeros.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        while(i<n)
+        {
+            if(nums[i]==0)
+            {
+              int j = i + 1;
+                while (j < n && nums[j] == 0) {
+                    j++;  
+                }
+                if (j < n) {
+                    swap(nums[i], nums[j]); 
+                } else {
+                    break;  
+                }
+            }
+         i++;
+        }
+        
+    }
+};
