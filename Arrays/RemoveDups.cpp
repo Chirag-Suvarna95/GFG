89c2d5458676697b749216120d7ad8552e5f6081
Chirag <chiragsuvarna95@gmail.com>
2024-10-07 22:00:10 +0530

    Optimal Sol:26. Remove Duplicates from Sorted Array

--- Arrays/RemoveDups.cpp
+++ Arrays/RemoveDups.cpp
@@ -12,3 +12,24 @@ public:
         return s.size();
     }
 };
+
+
+/*---------------Optimal Solution-----------------------*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int i=0,j;
+       int n=nums.size();
+
+       if(n==1)return nums[i];
+
+       for(j=1;j<n;j++)
+       {
+         if(nums[i]!=nums[j])
+         {
+            nums[++i]=nums[j];
+         }
+       }
+       return i+1;
+    }
+};
