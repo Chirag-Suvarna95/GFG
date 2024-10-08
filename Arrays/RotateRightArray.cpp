18274f3d0b04acbfb0433352f2e4b4364319c41f
Chirag <chiragsuvarna95@gmail.com>
2024-10-08 22:30:15 +0530

    189. Rotate Array(Optimal solution)

--- Arrays/RotateRightArray.cpp
+++ Arrays/RotateRightArray.cpp
@@ -24,3 +24,18 @@ public:
         }
     }
 };
+
+/*---------------Optimal Solution--------------------*/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k % n;
+
+        reverse(nums.begin(), nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(), nums.end());
+
+        
+    }
+};
