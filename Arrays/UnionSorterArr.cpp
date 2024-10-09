cb507a4c86d924bcec15b6e48966ae81a25e33d7
Chirag <chiragsuvarna95@gmail.com>
2024-10-09 22:43:21 +0530

    Union of Two Sorted Arrays: (Optimal Solution)

--- Arrays/UnionSorterArr.cpp
+++ Arrays/UnionSorterArr.cpp
@@ -22,3 +22,54 @@ class Solution{
         return a;
     }
 };
+
+/*--------------------Optimal Solution------------------------ */
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        //Your code here
+        //return vector with correct order of elements
+       vector<int> vec;
+       int j=0,i=0;
+       
+       while(i<n && j<m)
+       {
+           if(arr1[i]<=arr2[j])
+           {
+               if(vec.size()==0 || vec.back()!=arr1[i])
+                   vec.push_back(arr1[i]);
+                   
+              i++;
+           }
+           else
+               {
+                 if(vec.size()==0 || vec.back()!=arr2[j])
+                 vec.push_back(arr2[j]);
+                 
+                 j++;
+               } 
+       }
+       //Remaining elements
+       while(i<n)
+       {
+           if (vec.back() != arr1[i])
+             vec.push_back(arr1[i]);
+            
+            i++; //has to be written outside the if condition
+       }
+       
+       while(j<m)
+       {
+           if (vec.back() != arr2[j])
+             vec.push_back(arr2[j]);
+             
+            j++;
+       }
+       
+       return vec;
+    }
+};
