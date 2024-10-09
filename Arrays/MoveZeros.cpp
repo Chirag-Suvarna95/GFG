9aa82b399cf6c601ab41a5eb4923811aed4f4a56
Chirag <chiragsuvarna95@gmail.com>
2024-10-09 20:26:43 +0530

    Move zeros to end : GFG key if loop difference

--- Arrays/MoveZeros.cpp
+++ Arrays/MoveZeros.cpp
@@ -22,3 +22,39 @@ public:
         
     }
 };
+
+
+/*--------------GFG---------------*/
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int i=0;
+        int n=arr.size();
+        int j=0;
+        
+        while(i<n)
+        {
+            if(arr[i]==0) //Changes required only if its zero
+            {
+                if(j==0) //This if condition makes a difference
+                {j=i;}
+                
+                while(arr[j]==0  )
+                {
+                    j++;
+                    if (j == n) return;
+                }
+                if(j<n)
+                {
+                
+                    arr[i]=arr[j];
+                    arr[j]=0;
+                }
+                else break;
+                
+            }
+            i++;
+        }
+    }
+};
