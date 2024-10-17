de3efc667c614c610ce04446729bf8ffa63e1794
Chirag <chiragsuvarna95@gmail.com>
2024-10-17 22:18:17 +0530

    121. Best Time to Buy and Sell Stock (Optimal Solution)

--- Arrays/StockBuySell.cpp
+++ Arrays/StockBuySell.cpp
@@ -21,3 +21,21 @@ public:
         return profit;
     }
 };
+
+/*--------------------Optimal Solution------------------------------*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int profit=0;
+        int Min=prices[0];
+
+        for(int i=1;i<n;i++)
+        {
+            int cost=prices[i]-Min; //Compare current with prev minimum
+            profit=max(profit,cost); //Compare current profit with prev profit
+            Min=min(prices[i],Min); //Updates the previous minimum
+        }
+        return profit;
+    }
+};
